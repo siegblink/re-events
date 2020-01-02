837f4dd8fdde5699ba81fa16d0ba32f358321101
Sieg <siegblink@gmail.com>
2020-01-02 20:34:42 +0800

    Add SignedInMenu and SignedOutMenu components

--- src/features/nav/NavBar/NavBar.jsx
+++ src/features/nav/NavBar/NavBar.jsx
@@ -1,9 +1,26 @@
 import React, { Component } from 'react'
 import { Menu, Container, Button } from 'semantic-ui-react'
 import { NavLink, Link } from 'react-router-dom'
+import SignedOutMenu from '../Menus/SignedOutMenu'
+import SignedInMenu from '../Menus/SignedInMenu'
 
 class NavBar extends Component {
+  state = {
+    authenticated: true,
+  }
+
+  handleSignIn = () => {
+    this.setState({ authenticated: true })
+  }
+
+  handleSignOut = () => {
+    this.setState({ authenticated: false })
+    this.props.history.push('/')
+  }
+
   render() {
+    const { authenticated } = this.state
+
     return (
       <Menu inverted fixed='top'>
         <Container>
@@ -14,12 +31,20 @@ class NavBar extends Component {
           <Menu.Item as={NavLink} to='/events' name='Events' />
           <Menu.Item as={NavLink} to='/people' name='People' />
           <Menu.Item>
-            <Button as={Link} to='/createEvent' floated='right' positive inverted content='Create Event' />
+            <Button
+              as={Link}
+              to='/createEvent'
+              floated='right'
+              positive
+              inverted
+              content='Create Event'
+            />
           </Menu.Item>
-          <Menu.Item position='right'>
-            <Button basic inverted content='Login' />
-            <Button basic inverted content='Sign Out' style={{ marginLeft: '0.5em' }} />
-          </Menu.Item>
+          {authenticated ? (
+            <SignedInMenu signOut={this.handleSignOut} />
+          ) : (
+            <SignedOutMenu signIn={this.handleSignIn} />
+          )}
         </Container>
       </Menu>
     )
